9f46ac1d1e1e880b4e3fb1d0747a679cba7412fc
L BEZ <luka.bezovsek@gmail.com>
2016-11-04 23:57:41 +0100

    Login fixing, initial validation

--- engine.js
+++ engine.js
@@ -8,13 +8,29 @@ window.onload = function () {
     var loginButton = document.getElementById('loginButton');
     var loginDiv = document.getElementById('login');
     var mainDiv = document.getElementById('main');
+    var userInput = document.getElementById('userInput');
+    var passInput = document.getElementById('passInput');
 
-
-    loginButton.onclick = function() {
-        console.log("ININININ");
-        if(loginDiv.style.visibility !== 'hidden'){
-            loginDiv.style.visibility = 'hidden';
-            mainDiv.style.visibility = 'visible';
+    loginButton.onclick = function () {
+        var check = validateInputs();
+        console.log(check);
+        if (check) {
+            if (loginDiv.style.visibility !== 'hidden') {
+                loginDiv.style.visibility = 'hidden';
+                mainDiv.style.visibility = 'visible';
+            }
         }
+    };
+
+    validateInputs = function () {
+        if (userInput.value === '' || userInput.value === userInput.defaultValue) {
+            return false;
+        }
+        if (passInput.value === '' || passInput.value === passInput.defaultValue) {
+            return false;
+        }
+        return true;
     }
+
+
 }
