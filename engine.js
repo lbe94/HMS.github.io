26bf7ddb8905fccbdc6bd04fbfc391db7dc4652e
L BEZ <luka.bezovsek@gmail.com>
2016-11-04 22:44:23 +0100

    Created login screen

--- engine.js
+++ engine.js
@@ -1,3 +1,20 @@
 /**
  * Created by Luka on 28.10.2016.
  */
+
+
+window.onload = function () {
+    // elements declaration
+    var loginButton = document.getElementById('loginButton');
+    var loginDiv = document.getElementById('login');
+    var mainDiv = document.getElementById('main');
+
+
+    loginButton.onclick = function() {
+        console.log("ININININ");
+        if(loginDiv.style.visibility !== 'hidden'){
+            loginDiv.style.visibility = 'hidden';
+            mainDiv.style.visibility = 'visible';
+        }
+    }
+}
